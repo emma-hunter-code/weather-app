c8a9b75876e30cba8df845bafe77f0a058fd73b5
Emma Hunter <mmrbrts86@gmail.com>
2025-02-23 12:26:31 +0000

    Issue with temperatureElement inner html - still working on it

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temp-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "a2ab500f41eaff40b9aat33a8a92f8ao";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#city-search");
