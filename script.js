eaa1e782817b605526f9c176ff2a0fc826d0a490
Emma Hunter <mmrbrts86@gmail.com>
2025-02-28 22:35:23 +0000

    Added forecast JS in full

--- script.js
+++ script.js
@@ -83,27 +83,33 @@ function getForecast(city) {
   axios.get(apiUrl).then(displayForecast);
 }
 
-function displayForecast(response) {
-  console.log(response.data);
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
     <div class="forecast-days">
-      <div class="forecast-day">${day}</div>
-      <div class="forecast-icon">⛅️</div>
+      <div class="forecast-day">${formatDay(day.time)}</div>
+      <img src="${day.condition.icon_url}" class="forecast-icon" />
       <div class="forecast-temps">
         <div class="forecast-temp">
-          <strong>16°</strong>
+          <strong>${Math.round(day.temperature.maximum)}°</strong>
         </div>
-        <div class="forecast-temp">9°</div>
+        <div class="forecast-temp">${Math.round(day.temperature.minimum)}°</div>
       </div>
     </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
